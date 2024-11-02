ee1fbdbd1ae170b43b6dbc833abb62146405cee1
Biniam Aregay <seifubiniam6@gmail.com>
2024-11-03 01:30:44 +0300

    build the star rating

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import StarRating from "./StarRating";
+//import "./index.css";
+//import App from "./App";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This Moviw was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}{" "}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} />
+    <StarRating defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
